fe6c66df924fb6e03890dd51e41c3e5f44aa7bc4
rajosingh-adeptia <rajo.singh@adeptia.com>
2020-08-15 21:40:27 +0530

    First Commit

---
+++ src/main/java/com/adeptia/ApacheFOP/App.java
@@ -0,0 +1,13 @@
+package com.adeptia.ApacheFOP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
